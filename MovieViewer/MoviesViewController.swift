62a20e5f08444ee0ca84a9d91edeed7a31fb2fce
Andy Jiang <andyjiang@gmail.com>
2016-10-17 12:04:13 -0400

    Added ui table view

---
+++ MovieViewer/MoviesViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MoviesViewController.swift
+//  MovieViewer
+//
+//  Created by Andy Jiang on 10/17/16.
+//  Copyright © 2016 andyjiang. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        cell.textLabel?.text = "row \(indexPath.row)"
+        print("row \(indexPath.row)")
+        return cell
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
